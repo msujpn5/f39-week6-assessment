db5cf9d758fb3148b977ceca904fee9f30846495
Rex Holmes <holmesrex515@gmail.com>
2023-05-20 04:30:03 +0900

    added site address to ec2.md

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -18,12 +18,14 @@ describe("Duel Duo tests", () => {
 
   test('can display choices div', async () => {
     await driver.get("https://localhost:8000")
+    await driver.wait(until.titleIs("Duel Duo"), 1000)
     await driver.findElement(By.name("draw")).click()
     await driver.findElement(By.name("choices"))
   })
 
   test('can display player-duo div', async () => {
     await driver.get("https://localhost:8000")
+    await driver.wait(until.titleIs("Duel Duo"), 1000)
     await driver.findElement(By.name("draw")).click()
     await driver.findElement(By.name("bot-btn")).click()
     await driver.findElement(By.name("player-duo"))
--- ec2.md
+++ ec2.md
@@ -1,2 +1,3 @@
 ## Paste your link below
 
+http://54.176.89.112
